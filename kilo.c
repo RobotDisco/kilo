6bb1afe070a7e46c8c4d2b5fe322fb1c66530e41
Gaelan D'costa <gdcosta@gmail.com>
2018-01-08 21:18:50 -0500

    Support scrolling up/down the edited file

--- kilo.c
+++ kilo.c
@@ -15,7 +15,7 @@
 #include <unistd.h>
 
 /*** defines ***/
-#define KILO_VERSION "0.0.65"
+#define KILO_VERSION "0.0.69"
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -40,6 +40,7 @@ typedef struct erow {
 
 struct editorConfig {
   int cx, cy;
+  int rowoff;
   int screenrows;
   int screencols;
   int numrows;
@@ -213,10 +214,21 @@ void abFree(struct abuf *ab) {
 }
 
 /*** output ***/
+
+void editorScroll() {
+  if (E.cy < E.rowoff) {
+    E.rowoff = E.cy;
+  }
+  if (E.cy >= E.rowoff + E.screenrows) {
+    E.rowoff = E.cy - E.screenrows + 1;
+  }
+}
+
 void editorDrawRows(struct abuf *ab) {
   int y;
   for(y = 0; y < E.screenrows; y++) {
-    if (y >= E.numrows) {
+    int filerow = y + E.rowoff;
+    if (filerow >= E.numrows) {
       if (E.numrows == 0 && y == E.screenrows / 3) {
 	char welcome[80];
 	int welcomelen = snprintf(welcome, sizeof(welcome),
@@ -233,9 +245,9 @@ void editorDrawRows(struct abuf *ab) {
 	abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row[y].size;
+      int len = E.row[filerow].size;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row[y].chars, len);
+      abAppend(ab, E.row[filerow].chars, len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -246,6 +258,8 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorRefreshScreen() {
+  editorScroll();
+  
   struct abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6);
@@ -283,7 +297,7 @@ void editorMoveCursor(int key) {
     }
     break;
   case ARROW_DOWN:
-    if (E.cy != E.screenrows - 1) {
+    if (E.cy < E.numrows) {
       E.cy++;
     }
     break;
@@ -329,6 +343,7 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.rowoff = 0;
   E.numrows = 0;
   E.row = NULL;
   
